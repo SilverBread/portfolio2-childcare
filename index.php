7205b94842fc74f77c1417e3c76a7dd27c60fc9a
SilverBread <98303464+SilverBread@users.noreply.github.com>
2022-02-22 11:35:09 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Sample保育園 -職員連絡サイト-</title>
+    <link href="css/index.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <main>
+      <div class="main">
+        <h1>Sample保育園</h1>
+        <h2>---職員連絡サイト---</h2>
+        <p><a class="login" href="login.php">ログイン</a></p>
+      </div>
+    </main>
+  </body>
+</html>
