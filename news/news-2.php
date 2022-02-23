d5c568f821421ac03a4c5ed2cd4acca2d3ae33ba
SilverBread <98303464+SilverBread@users.noreply.github.com>
2022-02-23 18:55:00 +0900

    Update and rename news-2.php to news/news-2.php

--- news/news-2.php
+++ news/news-2.php
@@ -33,10 +33,10 @@
       <div class="normal">
         <nav>
           <ul class="global-nav">
-            <li><a href="top.php">ホーム</a></li>
-            <li><a href="news.php">引き続き</a></li>
-            <li><a href="buy.php">必要なもの</a></li>
-            <li><a href="posts.php">投稿</a></li>
+            <li><a href="../top.php">ホーム</a></li>
+            <li><a href="news.php">引き継ぎ</a></li>
+            <li><a href="../buy/buy.php">必要なもの</a></li>
+            <li><a href="../posts/posts.php">投稿</a></li>
             <!-- ログアウトボタン -->
             <li>
               <form action="logout.php" method="POST">
@@ -53,13 +53,13 @@
           </label>
         <nav class="nav">
           <ul class="nav-list">
-            <li><a href="top.php">ホーム</a></li>
+            <li><a href="../top.php">ホーム</a></li>
             <li><a href="news.php">引き継ぎ</a></li>
-            <li><a href="buy.php">必要なもの</a></li>
-            <li><a href="posts.php">投稿</a></li>
+            <li><a href="../buy/buy.php">必要なもの</a></li>
+            <li><a href="../posts/posts.php">投稿</a></li>
             <!-- ログアウトボタン -->
             <li>
-              <form action="logout.php" method="POST">
+              <form action="../logout.php" method="POST">
                 <input type="submit" name="logout" class="Logout-Btn" value="ログアウト">
               </form>
             </li>
